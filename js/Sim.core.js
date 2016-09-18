1df4c6935a0f552adbb4810bd033f0d440964899
Max Rubenacker <shuuki@gmail.com>
2016-09-18 11:26:51 -0600

    cleaning up internal values, preparing for events

--- js/Sim.core.js
+++ js/Sim.core.js
@@ -10,25 +10,36 @@ var Sim = {
 	// proof of life
 	test: function()
 	{
-		console.log('Sim ready.');
+		console.log('Sim ready');
 	},
 	// first steps
-	// initialize canvas
 	// takes an object with starting values
 	init: function(settings)
 	{
-		// start the clock
-		this.time = 0;
-		this.running = null;
-	
-		// set up canvas
+		// reset the clock
+		this._time = {
+			delta: 0,
+			now: 0,
+			up: 0,
+			running: false
+		}
+
+		// default settings
+		var fallback = {
+			id: 'display',
+			width: 480,
+			height: 300,
+			seed: {}
+		}
+
+		// initialize canvas
 		var canvas = document.createElement('canvas');
-		canvas.id = settings.id || 'viewport';
-		canvas.width = settings.width || 480;
-		canvas.height = settings.height || 300;
+		canvas.id = settings.id || fallback.id;
+		canvas.width = settings.width || fallback.width;
+		canvas.height = settings.height || fallback.height;
 
 		// declare entities to simulate
-		this.seed = settings.seed || {};
+		this._seed = settings.seed|| fallback.seed;
 
 		// only add canvas to body if one is not already present
 		if (!document.getElementById(canvas.id))
@@ -41,37 +52,38 @@ var Sim = {
 		this.canvas = document.getElementById(canvas.id);
 		this.context = this.canvas.getContext('2d');
 	},
+	// start running updates
 	start: function()
 	{
-		if (!this.running)
+		if (!this._time.running)
 		{
 			this.update();
 		}
 	},
+	// stop running updates
 	stop: function()
 	{
-		window.cancelAnimationFrame(this.running);
-		this.running = null;
+		window.cancelAnimationFrame(this._time.running);
+		this._time.running = false;
 	},
-	// update
 	// main loop heartbeat
 	update: function()
 	{
 		// start the next frame
-		this.running = requestAnimationFrame(this.update.bind(this));
+		this._time.running = requestAnimationFrame(this.update.bind(this));
 
 		// update clock
 		var now = new Date().getTime();
-		this.delta = (now - (this.time || now));
-		this.time = now;
+		this._time.delta = (now - (this._time.now || now));
+		this._time.now = now;
+		this._time.up += this._time.delta;
 
-		// update entities with delta
-		//this.entities.updateAll(this.delta);
+		// run updates for current time
+		this.event.updateAll(this._time);
 
 		// render scene
 		this.render();
 	},
-	// render
 	render: function()
 	{
 		// clear frame
@@ -80,52 +92,74 @@ var Sim = {
 		this.context.beginPath()
 
 		// add some debug text
-		var debug = this.time + ' ' + (1000 / this.delta).toFixed(0) + 'fps';
+		var debug = this._time.now + ' ' + (1000 / this._time.delta).toFixed(0) + 'fps';
 		this.context.font = '12px monospace';
 		this.context.fillStyle = 'gray';
 		this.context.fillText(debug, 12, 12);
 
-		// render entities
-		//this.entities.drawAll(this.context);
+		// draw all entities in event queue
+		this.event.drawAll(this.context);
 
 		// close out drawing
 		this.context.fill();
 		this.context.stroke();
 		this.context.closePath();
 	},
+
 	save: function()
 	{
-		this.cache =
-		{
+		this._cache = {
 			// add sim state values here for save/load
 			//scene: this.scene,
-			time: this.time
+			time: this._time
 		};
-		localStorage.setItem('SimSave', JSON.stringify(this.cache));
+		localStorage.setItem('SimSave', JSON.stringify(this._cache));
 	},
 	load: function()
 	{
 		if (localStorage.getItem('SimSave'))
 		{
-			this.cache = JSON.parse(localStorage.getItem('SimSave'));
+			this._cache = JSON.parse(localStorage.getItem('SimSave'));
 		}
 	},
 	reset: function()
 	{
 		// clear cache
-		if (this.cache)
+		if (this._cache)
 		{
-			this.cache = null;
+			this._cache = null;
 		}
 		// remove locally stored data if present
 		if (localStorage.getItem('SimSave'))
 		{
 			localStorage.removeItem('SimSave');
 		}
+		this.init();
 	},
-  entities: {
+
+  event:
+	{
+
+		queue: [],
+
+
+		enqueue: function(data)
+		{
+			this.queue.push(data);
+			// queue
+
+			// add event
+			// go through queue
+			// while (queue.length < 0) {  }
+
+		},
+
+		dequeue: function()
+		{
+			this.queue.shift();
+		},
+
     
-    // entities being simulated
     active: {},
 
 		assign: function(id, values)
@@ -135,29 +169,43 @@ var Sim = {
 		destroy: function(id)
 		{
 			delete this.active[id];
-		},		
+		},
 		update: function(id, args)
 		{
       this.active[id].update(args)
     },
 		updateAll: function(delta)
 		{
+			// build a queue
+			// go through it all from top to bottom
+			// something better than this:
 			//foreach active id 
 			//this.active[id].update(delta);
 		},
+
 		draw: function(id, args)
 		{
 			this.active[id].draw(args)
 		},
 		drawAll: function(context)
 		{
+			// 
 			//foreach active id 
 			//this.active[id].draw(context);
 		},
 
-		stage: {},
-    entities: {},
-		views: {},
+  }
+}
+
+
+
+
+
+		// entities being simulated
+
+		//stage: {},
+    //entities: {},
+		//views: {},
 
 		// view entity needs what values?
 		/*
@@ -169,5 +217,3 @@ var Sim = {
 		  aspect: 1.6 || (this.width / this.height)
 		}
 		*/
-  }
-}
