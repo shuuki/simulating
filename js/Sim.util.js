b35bb676f8f2f1bf21bdf8e95480659ec8b44db3
Max Rubenacker <shuuki@gmail.com>
2016-09-22 12:42:53 -0600

    separated randomness from utilities

--- js/Sim.util.js
+++ js/Sim.util.js
@@ -1,9 +1,9 @@
 /** utilities */
 
 // provides useful tools and math
-// stores pi for faster calculations
+// store pi for faster calculations
 // degree / radian converter
-// randomness inspired by dice and cards
+// some trig
 
 Sim.util = {
 	// cache pi for speed
@@ -18,32 +18,9 @@ Sim.util = {
 	{
 		return radians * (180 / this.pi);
 	},
-	// roll a random whole number between 0 and a maximum
-	roll: function(max)
+	// calculate angle between two points
+	boop: function(x1, y1, x2, y2)
 	{
-		return Math.floor(Math.random() * max);
-	},
-	// pick a random element from an array
-	pick: function(list)
-	{
-		return list[Math.floor(Math.random() * list.length)];
-	},
-	// shuffle
-	// from mike bostock via frankmitchell.org/2015/01/fisher-yates
-	shuffle: function(list)
-	{
-		var m = list.length,
-			t, i;
-		// while there are elements to shuffle...
-		while (m)
-		{
-			// pick a remaining element...
-			i = Math.floor(Math.random() * m--);
-			// and swap it with the current element
-			t = list[m];
-			list[m] = list[i];
-			list[i] = t;
-		}
-		return list;
+		return Math.atan2(y2 - y1, x2 - x1) * 180 / this.pi + 180;
 	}
 }
