73a19a491532e4851aa9cfb5c589951d20031206
Max Rubenacker <shuuki@gmail.com>
2016-09-26 10:54:49 -0600

    worked on craft dynamics, trying to get weighted vectors right

--- js/demo.voyager.js
+++ js/demo.voyager.js
@@ -17,6 +17,7 @@ var voyager = {
 		height: 640
 	},
 	seed: {
+		ship: new Craft(40,20),
 		ticker: new Ticker(),
 		//camera: new Camera(),
 		//stage: new Stage(),
@@ -26,131 +27,18 @@ var voyager = {
 		// http://nssdc.gsfc.nasa.gov/planetary/factsheet/
 		//             10^24 kg,       km,   10^6 km,  km/s,       hex    //   10^6 km,  10^6 km,
 	 	//                 mass, diameter,  distance, speed,     color    // periapsis, apoapsis,
-	 	slvr: new Body(       0,9999999999,        0,     0, '#282828' ), //         0,        0,
-		sonn: new Body( 1988435,   695700,         0,     0, '#f8f8f2' ), //         0,        0,
+	 	//slvr: new Body(       0,100000000,         0,     0, '#282828' ), //         0,        0,
+		sonn: new Body( 1988435,   69570.0,         0,     0, '#f8f8f2' ), //         0,        0,
 		mirk: new Body(   0.330,     4879,      57.9,  47.4, '#787878' ), //      46.0,     69.8,
 		vans: new Body(    4.87,    12104,     108.2,  35.0, '#ae885d' ), //     107.5,    108.9,
 		erth: new Body(    5.97,    12756,     149.6,  29.8, '#888ba0' ), //     147.1,    152.1,
 		marz: new Body(   0.642,     6792,     227.9,  24.1, '#86674c' ), //     206.6,    249.2,
-		jupt: new Body(    1898,   142984,     778.6,  13.1, '#b87e1b' ), //     740.5,    816.6,
-		sats: new Body(     568,   120536,    1433.5,   9.7, '#debc7f' ), //    1352.6,   1514.5,
-		urns: new Body(    86.8,    51118,    2872.5,   6.8, '#a8ccd1' ), //    2741.3,   3003.6,
-		nepz: new Body(     102,    49528,    4495.1,   5.4, '#3d5ad8' ), //    4444.5,   4545.7,
+		//jupt: new Body(    1898,   142984,     778.6,  13.1, '#b87e1b' ), //     740.5,    816.6,
+		//sats: new Body(     568,   120536,    1433.5,   9.7, '#debc7f' ), //    1352.6,   1514.5,
+		//urns: new Body(    86.8,    51118,    2872.5,   6.8, '#a8ccd1' ), //    2741.3,   3003.6,
+		//nepz: new Body(     102,    49528,    4495.1,   5.4, '#3d5ad8' ), //    4444.5,   4545.7,
 	}
 }
-
-
-// celestial bodies
-
-function Body(mass, diameter, distance, speed, color) // periapsis, apoapsis
-{
-
-	var scale = {
-		distance: 1,
-		diameter: .0001,
-		time: 0.0005
-	}
-
-	this.m = mass;
-	this.d = diameter * scale.diameter; // radius
-	this.od = distance * scale.distance; // orbit, distance from origin
-	this.s = speed * scale.time; // speed
-
-	this.color = color; 
-
-	this.p = 0; // period, how far along
-
-	//this.l = l; // length of 
-
-
-// origin x/y, only for rendering
-	this.ox = 240;
-	this.oy = 320;
-}
-
-Body.prototype.update = function()
-{
-	
-	this.p += this.s;
-
-	var whatsit = blah(this.od, this.p);
-	
-	this.x = whatsit[0]+this.ox
-	this.y = whatsit[1]+this.oy
-	
-	//https://lord.io/blog/2014/kepler/
-
-
-	
-
-
-	// two coords in [x,y] format
-	function distance(a, b)
-	{
-		var distance = Math.sqrt(Math.pow(b[1] - a[1], 2) + Math.pow(b[0] - a[0], 2));
-	  return distance;
-	}
-
-
-	function blah(length, angle)
-	{
-		var x = length * Math.cos(angle),
-				y = length * Math.sin(angle);
-		return [x,y];
-	}	
-
-	function attenuate(intensity, distance)
-	{
-	  return (intensity / Math.pow(distance, 2));
-	}
-
-
-
-	function Vector (x, y, z)
-	{
-	  this.x = x;
-	  this.y = y;
-	  this.z = z;
-	}
-
-
-}
-Body.prototype.draw = function(origin)
-{
-	var context = origin.context;
-
-	// fill circle with body color
-	context.beginPath();
-	context.fillStyle = this.color;
-	context.arc(this.x, this.y, this.d/2, 0, 2 * Math.PI);
-	context.globalAlpha = 1;
-	context.fill();
-	context.closePath();
-
-	// stroke outline for small bodies
-	context.beginPath();
-	context.strokeStyle = this.color;
-	context.lineWidth = 4;
-	context.arc(this.x, this.y, 8, 0, 2 * Math.PI);
-	context.globalAlpha = 0.2;
-	context.stroke();
-	context.closePath();
-
-	// cycles elapsed on each planet
-	//context.globalAlpha = 1;
-	//context.fillText((this.d/(2*Math.PI)).toFixed(1), this.x+8, this.y+4);
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
 // little thing that ticks in one second increments
@@ -168,4 +56,248 @@ Ticker.prototype.draw = function(origin)
 {
 	var context = origin.context;
 	context.fillText(this.time.toFixed(0), 12, (context.canvas.height - 12));
+}
+
+
+// celestial bodies
+
+function Body(mass, diameter, distance, speed, color) // periapsis, apoapsis
+{
+	var scale = {
+		distance: 1,
+		diameter: 0.0005,
+		time: 0.00001
+	}
+
+	this.m = mass;
+	this.d = diameter * scale.diameter; // radius
+	this.od = distance * scale.distance; // orbit, distance from origin
+	this.speed = speed * scale.time; // speed
+	this.color = color; 
+
+	this.p = 0; // period, how far along
+
+	// origin x/y, only for rendering
+	this.ox = 240;
+	this.oy = 320;
+}
+Body.prototype.update = function(origin)
+{
+	
+	this.p += this.speed;
+
+	var whatsit = blah(this.od, this.p);
+	
+	this.x = whatsit[0]+this.ox
+	this.y = whatsit[1]+this.oy
+	
+	//https://lord.io/blog/2014/kepler/
+
+}
+Body.prototype.draw = function(origin)
+{
+	var context = origin.context;
+
+	// fill circle with body color
+	context.beginPath();
+	context.fillStyle = "black";
+	context.rect(0, 0, context.canvas.width, context.canvas.height);
+	context.globalAlpha = 1;
+	context.fill();
+	context.closePath();
+
+	// fill circle with body color
+	context.beginPath();
+	context.fillStyle = this.color;
+	context.arc(this.x, this.y, this.d/2, 0, 2 * Math.PI);
+	context.globalAlpha = 1;
+	context.fill();
+	context.closePath();
+
+	// stroke outline for small bodies
+	context.beginPath();
+	context.strokeStyle = this.color;
+	context.lineWidth = 4;
+	context.arc(this.x, this.y, 8, 0, 2 * Math.PI);
+	context.globalCompositeOperation = 'screen';
+	context.globalAlpha = 0.2;
+	context.stroke();
+	context.closePath();
+
+	// cycles elapsed on each planet
+	//context.globalAlpha = 1;
+	//context.fillText((this.d/(2*Math.PI)).toFixed(1), this.x+8, this.y+4);
+}
+
+
+// space craft
+
+function Craft(x, y)
+{
+	this.x = x || 0;
+	this.y = y || 0;
+	this.t = 0;
+	this.history = [];
+	this.history.push({
+		x: this.x,
+		y: this.y,
+		t: this.t
+	});	
+}
+Craft.prototype.update = function(origin)
+{
+	// update internal time
+	this.t += (origin.time.delta/1000);
+	
+	// do x/y transforms
+	this.x +=  2 * origin.time.delta / 200
+	this.y +=  2 * origin.time.delta / 200
+	
+	// build a list of all bodies x/y with their masses
+	this.bodies = [];
+	this.bodiesTotalMass = 0;
+	for (var body in origin.entity.active)
+	{
+		var isType = origin.entity.active[body].constructor.name;
+		if (isType === 'Body')
+		{
+			var selected = origin.entity.active[body];
+			this.bodiesTotalMass += selected.m;
+			this.bodies.push({
+				x: selected.x,
+				y: selected.y,
+				m: selected.m
+			});
+		}
+	}
+	
+	// truncate history after 10000 steps
+	if (this.history.length > 10000) {
+		this.history.shift()
+	}
+
+	// add current update to history
+	this.history.push({
+		x: this.x,
+		y: this.y,
+		t: this.t
+	})
+
+}
+Craft.prototype.draw = function(origin)
+{
+	// bind canvas
+	var context = origin.context;
+
+	// fill circle with body color
+	context.beginPath();
+	context.fillStyle = '#f8f8f8';
+	context.arc(this.x, this.y, 1.5, 0, 2 * Math.PI);
+	context.globalAlpha = 1;
+	context.fill();
+	context.closePath();
+
+	// asteroids ship style
+	//var shipAngle = 0.8,
+	//shipSize = 8;
+	//context.strokeStyle = 'magenta';
+	//context.lineWidth = 2;
+	//context.arc(this.x, this.y, shipSize, (this.Direction - origin.util.pi - shipAngle), (this.Direction - origin.util.pi + shipAngle), false);
+	//context.arc(this.x, this.y, shipSize, this.Direction, this.Direction, false);
+	//context.arc(this.x, this.y, shipSize, (this.Direction - origin.util.pi - shipAngle), (this.Direction - origin.util.pi + shipAngle), false);
+
+	// average out a gravity vector
+	var vectorWeight = 0,
+		vectorAngle = 0;
+	var avX = 0, avY = 0;
+
+	for (var k = 0; k < this.bodies.length; k++)
+	{	
+		// calculate effect of gravity using body mass and distance		
+		var mass = this.bodies[k].m,
+			distance = length([this.x, this.y], [this.bodies[k].x, this.bodies[k].y]),
+			weight = attenuate(mass, distance);
+
+		vectorWeight += weight;
+
+		// calculate weighted vector averaging all bodies
+		avX += this.bodies[k].x// * weight;
+		avY += this.bodies[k].y// * weight;
+		
+
+		// draw a line to every body
+		context.beginPath();
+		context.moveTo(this.x, this.y)
+		context.lineTo(this.bodies[k].x, this.bodies[k].y);
+		context.strokeStyle = 'rgba(255,0,0,' + weight * 100 + ')';
+		//context.lineWidth = weight;
+		context.stroke();
+		context.closePath();
+	}
+
+	avX = avX/this.bodies.length;
+	avY = avY/this.bodies.length;
+	//console.log(avX,avY)
+
+	vectorAngle = angle([this.x, this.y], [avX, avY])
+	
+
+	
+	// draw current gravity vector
+	var oo = blah(vectorWeight, vectorAngle)
+	context.beginPath();
+	context.moveTo(this.x, this.y)
+	context.strokeStyle = 'rgba(255,255,255,0.5)';
+	context.lineWidth = 2;
+	context.lineTo(this.x + oo[0], this.y + oo[1]);
+	context.stroke();
+	context.closePath();
+
+	// draw a trail behind craft using points in history
+	context.beginPath();
+	context.fillStyle = '#444';
+	for (var i = 0; i < this.history.length; i++)
+	{
+		context.arc(this.history[i].x, this.history[i].y, i / this.history.length, 0, 2 * Math.PI);
+		//context.rect(this.history[i].x-1, this.history[i].y-1, 2, 2);
+	}
+	context.fill();
+	context.closePath();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// distance between two points in [x,y]
+function length(a, b) {
+	var length = Math.sqrt(Math.pow(b[1] - a[1], 2) + Math.pow(b[0] - a[0], 2));
+	return length;
+}
+
+// angle between two points in [x,y]
+function angle(a, b) {
+	var angle = Math.atan2(b[1] - a[1], b[0] - a[0])// * 180 / Math.PI + 180;
+	return angle;
+}
+
+//
+function blah(length, angle) {
+	var x = length * Math.cos(angle),
+		y = length * Math.sin(angle);
+	return [x, y];
+}
+
+// 
+function attenuate(intensity, length) {
+	var attenuation = intensity / Math.pow(length, 2);
+	return attenuation;
 }
