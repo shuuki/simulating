38e5218977fbf19415e00826b7f4e11e02c81792
Max Rubenacker <shuuki@gmail.com>
2016-09-26 13:07:06 -0600

    vectors closer to being accurate, last waypoint before refactoring

--- js/demo.voyager.js
+++ js/demo.voyager.js
@@ -28,15 +28,15 @@ var voyager = {
 		//             10^24 kg,       km,   10^6 km,  km/s,       hex    //   10^6 km,  10^6 km,
 	 	//                 mass, diameter,  distance, speed,     color    // periapsis, apoapsis,
 	 	//slvr: new Body(       0,100000000,         0,     0, '#282828' ), //         0,        0,
-		//sonn: new Body( 1988435,   69570.0,         0,     0, '#f8f8f2' ), //         0,        0,
+		sonn: new Body( 1988435,   69570.0,         0,     0, '#f8f8f2' ), //         0,        0,
 		mirk: new Body(   0.330,     4879,      57.9,  47.4, '#787878' ), //      46.0,     69.8,
 		vans: new Body(    4.87,    12104,     108.2,  35.0, '#ae885d' ), //     107.5,    108.9,
 		erth: new Body(    5.97,    12756,     149.6,  29.8, '#888ba0' ), //     147.1,    152.1,
 		marz: new Body(   0.642,     6792,     227.9,  24.1, '#86674c' ), //     206.6,    249.2,
-		//jupt: new Body(    1898,   142984,     778.6,  13.1, '#b87e1b' ), //     740.5,    816.6,
-		//sats: new Body(     568,   120536,    1433.5,   9.7, '#debc7f' ), //    1352.6,   1514.5,
-		//urns: new Body(    86.8,    51118,    2872.5,   6.8, '#a8ccd1' ), //    2741.3,   3003.6,
-		//nepz: new Body(     102,    49528,    4495.1,   5.4, '#3d5ad8' ), //    4444.5,   4545.7,
+		jupt: new Body(    1898,   142984,     778.6,  13.1, '#b87e1b' ), //     740.5,    816.6,
+		sats: new Body(     568,   120536,    1433.5,   9.7, '#debc7f' ), //    1352.6,   1514.5,
+		urns: new Body(    86.8,    51118,    2872.5,   6.8, '#a8ccd1' ), //    2741.3,   3003.6,
+		nepz: new Body(     102,    49528,    4495.1,   5.4, '#3d5ad8' ), //    4444.5,   4545.7,
 	}
 }
 
@@ -218,9 +218,14 @@ Craft.prototype.draw = function(origin)
 			distance = length([this.x, this.y], [this.bodies[k].x, this.bodies[k].y]),
 			weight = attenuate(mass, distance),
 			multiplier = mass / this.bodiesTotalMass;
+			//console.log(weight)
 
 		vectorWeight += weight;
 
+
+		// multiplier should be based on total current weight, not overall system mass
+		// so near low mass bodies can be more influential than distant high mass bodies
+	
 		// calculate weighted vector averaging all bodies
 		avX += this.bodies[k].x * multiplier;
 		avY += this.bodies[k].y * multiplier;
@@ -244,7 +249,7 @@ Craft.prototype.draw = function(origin)
 
 	
 	// draw current gravity vector
-	var oo = blah(200, vectorAngle)
+	var oo = blah(59, vectorAngle)
 	context.beginPath();
 	context.moveTo(this.x, this.y)
 	context.strokeStyle = 'rgba(255,255,255,0.5)';
