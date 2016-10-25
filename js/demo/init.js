395be15ac310f8ab82044ce89f8b0c56fc58b166
Max Rubenacker <shuuki@gmail.com>
2016-10-25 13:32:47 -0600

    simplified init

--- js/demo/init.js
+++ js/demo/init.js
@@ -1,12 +1,7 @@
+
+
 var landscape = Object.create(Walk);
 landscape.init('plains');
 
-
-var world = {
-	landscape: Object.create(Walk).init('plains')
-}
-
-
 var sim = Object.create(Sim).init().start();
-
 sim.scene.add('landscape', landscape)
